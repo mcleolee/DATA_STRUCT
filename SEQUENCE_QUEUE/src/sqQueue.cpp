981958a022fbc6a604cdd9bdafa71c7728dc9042
mcleolee <59864232+mcleolee@users.noreply.github.com>
2022-07-23 14:33:49 +0800

    7.23 1433 整理

---
+++ SEQUENCE_QUEUE/src/sqQueue.cpp
@@ -0,0 +1,9 @@
+#include "sqQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+sqq_data_type init_sqQueue(sqq_ptr_node L)
+{
+    
+}
--- SEQUENCE_QUEUE/src/sqQueue.h
+++ SEQUENCE_QUEUE/src/sqQueue.h
@@ -1,12 +1,18 @@
 #ifndef _SQQUEUE_H
 #define _SQQUEUE_H
 
-typedef char sqq_data_t;
+#define N 1024
+
+typedef char sqq_data_type;
 typedef struct sqQueue
 {
-    sqq_data_t data;
-    struct sqQueue *next;
+    sqq_data_type arrData[N];
+    sqq_data_type data;
+    struct sqQueue *front;
+    struct sqQueue *rear;
 }sqq_node,*sqq_ptr_node;
+
+sqq_data_type init_sqQueue(sqq_ptr_node L);
 
 
 
